d6476632b31afee5198673186f667355fdc072f7
antweer <trajwani@gmail.com>
2017-05-23 21:28:06 -0500

    Working blackjack game without a restart feature

--- blackjack.js
+++ blackjack.js
@@ -1,5 +1,5 @@
 $(function domReady() {
-  var deck = [
+  var cards1 = [
     { point: 1, suit: 'hearts' },
     { point: 1, suit: 'spades' },
     { point: 1, suit: 'diamonds' },
@@ -53,8 +53,22 @@ $(function domReady() {
     { point: 13, suit: 'diamonds' },
     { point: 13, suit: 'clubs' },
   ];
+  var cards2 = cards1;
+  var deck = [];
   var dealerHand;
   var playerHand;
+  var hitCount = 2;
+  var standCount = 2;
+  function random(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+  function shuffle(array){
+    for(x=0; x<52; x++){
+      deck.push(array.splice(random(0,array.length),1));
+    }
+  }
   function getImage(object){
     var pointConversion = {
       1:'ace',
@@ -67,17 +81,90 @@ $(function domReady() {
       point = pointConversion[object.point];
     }
     else{
-      point = object.point
+      point = object.point;
     }
     return("/images/" + point + "_of_" + object.suit + ".png");
   }
+  function calculatePoints(array){
+    var points = 0;
+    for(x=0; x<array.length; x++){
+      if(array[x][0].point == 1){
+        if((21-points)<11){
+          points += 1;
+        } else {
+          points += 11;
+        }
+      }else if(array[x][0].point > 10){
+        points += 10;
+      }else{
+        points += array[x][0].point;
+      }
+    }
+    return points;
+  }
+  function determineWinner(){
+    if(calculatePoints(dealerHand) > calculatePoints(playerHand)){
+      $('#messages').text('You lose!');
+    }else if(calculatePoints(playerHand) > calculatePoints(dealerHand)){
+      $('#messages').text('You win!');
+    }else{
+      $('#messages').text("It's a draw!");
+    }
+  }
+  function restart(){
+    cards2 = cards1;
+    hitCount = 2;
+    standCount = 2;
+    deck = [];
+    dealerHand = [];
+    playerHand = [];
+    $('#dealer-hand').text('');
+    $('#player-hand').text('');
+    $('#restart-button').hide();
+    $('#deal-button').show();
+    $('#hit-button').show();
+    $('#stand-button').show();
+  }
+  $('#restart-button').hide();
   $('#deal-button').click(function(){
+    shuffle(cards2);
     dealerHand = [deck.pop(), deck.pop()];
     playerHand = [deck.pop(), deck.pop()];
-    $('#dealer-hand').append('<img src="' + getImage(dealerHand[0]) + '"/><img src="' + getImage(dealerHand[1]) + '"/>');
-    $('#player-hand').append('<img src="' + getImage(playerHand[0]) + '"/><img src="' + getImage(playerHand[1]) + '"/>');
+    $('#dealer-hand').append('<img src="' + getImage(dealerHand[0][0]) + '"/><img src="' + getImage(dealerHand[1][0]) + '"/>');
+    $('#player-hand').append('<img src="' + getImage(playerHand[0][0]) + '"/><img src="' + getImage(playerHand[1][0]) + '"/>');
+    $('#dealer-points').text(calculatePoints(dealerHand));
+    $('#player-points').text(calculatePoints(playerHand));
+    $('#deal-button').hide();
   });
   $('#hit-button').click(function(){
-    $('#player-hand').append('<img src="images/2_of_diamonds.png"/>');
+    playerHand.push(deck.pop());
+    $('#player-hand').append('<img src="' + getImage(playerHand[hitCount][0]) + '"/>');
+    $('#player-points').text(calculatePoints(playerHand));
+    if(calculatePoints(playerHand)>21){
+      $('#messages').text("You've busted! Dealer wins.")
+      $('#hit-button').hide();
+      $('#stand-button').hide();
+      $('#restart-button').show();
+    }
+    hitCount += 1;
+  });
+  $('#stand-button').click(function(){
+    $('#hit-button').hide();
+    while(calculatePoints(dealerHand)<17){
+      dealerHand.push(deck.pop());
+      $('#dealer-hand').append('<img src="' + getImage(dealerHand[standCount][0]) + '"/>');
+      $('#dealer-points').text(calculatePoints(dealerHand));
+      standCount += 1;
+    }
+    if(calculatePoints(dealerHand)>21){
+        $('#messages').text("Dealer has busted! You win!");
+    } else {
+      determineWinner();
+    }
+    $('#stand-button').hide();
+    $('#restart-button').show();
+  });
+  $('#restart-button').click(function(){
+    restart();
   });
 });
